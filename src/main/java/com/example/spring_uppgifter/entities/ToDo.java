e5e2e9a7b581e129289ebe4efb7edfa9be9ef6ac
Haris Avdicevic <haris.avdi93@gmail.com>
2022-04-01 17:58:05 +0200

    Fixed Title & message bug.

--- src/main/java/com/example/spring_uppgifter/entities/ToDo.java
+++ src/main/java/com/example/spring_uppgifter/entities/ToDo.java
@@ -27,6 +27,18 @@ public class ToDo {
 
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
     public int getId() {
         return id;
     }
